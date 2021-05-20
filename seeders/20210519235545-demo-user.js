b7e0d94b5355c87798fcc2955e5b1c54d11a444a
Jafar <mjafarashsiddiq@gmail.com>
2021-05-20 08:12:09 +0700

    first commit add auth and setup the routers

---
+++ seeders/20210519235545-demo-user.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          email: 'test@mail',
+          fullName: 'John Doe',
+          password: '12345',
+          role: 'admin',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     *
+     */
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT;
+const route = require("./routes/index");
+const cors = require("cors");
+
+app.use(express.json());
+
+app.use(cors());
+
+app.use("/api/v1", route);
+
+app.use(express.json());
+
+app.listen(port, () => console.log(`listening on port ${port}`));
